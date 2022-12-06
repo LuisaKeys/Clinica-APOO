425f96b4f3b006d7224ad561c59b4bebc36ce342
Luisa Weydner De Lima Chaves <luisa.lima@academico.ifrn.edu.br>
2022-12-06 09:38:08 -0300

    <PETIDADE>

--- Modelo/Models/Pet.cs
+++ Modelo/Models/Pet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Modelo.Models
     {
         public long PetId { get; set; }
         public string Nome { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Apenas números positivos")]
         public int Idade { get; set; }
         public TipoSexo Sexo { get; set; }
         public int EspecieId { get; set; }
